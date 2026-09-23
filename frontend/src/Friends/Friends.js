9f458c1c6c62dc3a5b3208f55153106998ce86f5
Abdussalam <abdussalam30.1995@gmail.com>
2026-09-24 02:14:20 +0800

    use MAI web app referral link

--- frontend/src/Friends/Friends.js
+++ frontend/src/Friends/Friends.js
@@ -66,7 +66,7 @@ export default function Friends({
 
   const inviteLink =
     telegramId
-      ? `https://t.me/${botUsername}?startapp=r_${telegramId}`
+      ? `https://t.me/${botUsername}/app?startapp=r_${telegramId}`
       : '';
 
   const authHeaders = () => {
